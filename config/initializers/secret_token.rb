9ff85c37f558ee276564fe10ba996098e4e13e6e
Brayden Cleary <brayden.cleary@gmail.com>
2013-10-27 12:53:42 -0700

    First commit...initializing rails, skipping test-unit, adding pg database for heroku deployment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenderGuesser::Application.config.secret_token = 'd9b60280f58a11278fbbf6b8e86326fdd6e328dbe3b04b7943545c2f84091a40b1f5a9fd67e0a03f1eb9e65806e5db705ec02d8d4e1945dbe59a3f7ff063d74b'
