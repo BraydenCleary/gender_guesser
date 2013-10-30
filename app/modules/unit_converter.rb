51146f27ad85e6f05eb799192c7dac7643e22594
Brayden Cleary <brayden.cleary@gmail.com>
2013-10-29 21:19:50 -0700

    Removing unused methods from UnitConverter module

--- app/modules/unit_converter.rb
+++ app/modules/unit_converter.rb
@@ -4,15 +4,8 @@ module UnitConverter
     (lbs / 2.2046).round(2)
   end
 
-  def kgs_to_lbs(kgs)
-    (kgs * 2.2046).round(2)
-  end
-
   def inches_to_cms(inches)
     (inches / 0.39370).round(2)
   end
 
-  def cms_to_inches(cms)
-    (cms * 0.39370).round(2)
-  end
 end
